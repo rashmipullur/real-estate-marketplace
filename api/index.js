91da5731c60b09350c212fb88890727a4c76fe2a
rashmipullur <mheresince1998@gmail.com>
2024-05-06 12:13:57 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {})
+  .then(() => console.log("DB connection successful. 🫡"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
